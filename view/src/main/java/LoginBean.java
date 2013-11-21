22fa158ca61de567c273d0f9cbfb846dd98701d7
alexey.alekseenko <alexey.alekseenko@engagepoint.com>
2013-11-21 18:35:02 +0200

    integrated with bootstrap

--- view/src/main/java/LoginBean.java
+++ view/src/main/java/LoginBean.java
@@ -1,6 +1,7 @@
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.view.facelets.FaceletContext;
 import javax.servlet.http.HttpSession;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
@@ -57,10 +58,12 @@ public class LoginBean implements Serializable {
     public void setUrl(String url) {
         this.url = url;
     }
+
     public String doLogin() {
+
         String page = "";
         CMISTypeManagerService service = CMISTypeManagerService.getInstance();
-       // service = CMISTypeManagerService.getInstance();
+        service = CMISTypeManagerService.getInstance();
         service.setName(username);
         service.setPass(password);
         service.setPort(port);
