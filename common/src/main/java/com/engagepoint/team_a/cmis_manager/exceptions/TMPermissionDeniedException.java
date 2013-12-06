0618be759fddbedf36ee90a6aa3d66992af668d8
alexey.alekseenko <alexey.alekseenko@engagepoint.com>
2013-12-06 15:11:40 +0200

    new exception added

---
+++ common/src/main/java/com/engagepoint/team_a/cmis_manager/exceptions/TMPermissionDeniedException.java
@@ -0,0 +1,14 @@
+package com.engagepoint.team_a.cmis_manager.exceptions;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexey.alekseenko
+ * Date: 12/6/13
+ * Time: 11:50 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TMPermissionDeniedException extends TMBaseException{
+    public TMPermissionDeniedException(String message) {
+        super(message);
+    }
+}
