964018defcc600d7f7e5ed4c61a8ba2f68897395
alexey.alekseenko <alexey.alekseenko@engagepoint.com>
2013-12-13 13:16:18 +0200

    added download button

--- view/src/main/java/com/engagepoint/team_a/cmis_manager/FileDownloadController.java
+++ view/src/main/java/com/engagepoint/team_a/cmis_manager/FileDownloadController.java
@@ -1,25 +1,56 @@
 package com.engagepoint.team_a.cmis_manager;
 
-import java.io.InputStream;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletContext;
+import java.io.InputStream;
 
-import org.primefaces.model.DefaultStreamedContent;
-import org.primefaces.model.StreamedContent;
-@ManagedBean(name = "download")
+@ManagedBean
 @SessionScoped
 public class FileDownloadController {
 
     private StreamedContent file;
+    private String type;
+    private String[] types = {"xml", "json"};
 
     public FileDownloadController() {
-        InputStream stream = ((ServletContext)FacesContext.getCurrentInstance().getExternalContext().getContext()).getResourceAsStream("/WEB-INF/o.txt");
-        file = new DefaultStreamedContent(stream, "image/jpg", "downloaded_optimus.txt");
+    }
+
+    public String[] getTypes() {
+        return types;
+    }
+
+    public void setTypes(String[] types) {
+        this.types = types;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public StreamedContent getFile() {
         return file;
     }
+
+    public void setFile(StreamedContent file) {
+        this.file = file;
+    }
+
+    public void setURL() {
+        file = null;
+        String url = "/WEB-INF/web." + type;
+        String saveName = "downloaded_optimus." + type;
+        InputStream stream = ((ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext()).getResourceAsStream(url);
+        file = new DefaultStreamedContent(stream, "image/jpg", saveName);
+    }
 }
+
+
