19b26192c3181441d1571c5ecb89a2a1ba9308f1
teamAEngagePoint <kondr_vlad@mail.ru>
2013-12-16 14:46:56 +0200

    XML/JSON converter added v3

--- view/src/main/java/com/engagepoint/team_a/cmis_manager/FileUploadController.java
+++ view/src/main/java/com/engagepoint/team_a/cmis_manager/FileUploadController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import java.io.*;
@@ -15,6 +16,13 @@ public class FileUploadController {
     private String msgLbl = "----";
     private String show="false";
     private String fileExtension;
+    @ManagedProperty(value = "#{treeBean}")
+    private TreeBean treeBean;
+    public void setTreeBean(TreeBean treeBean) {
+        this.treeBean = treeBean;
+    }
+
+
     public String getShow() {
         return show;
     }
@@ -53,6 +61,7 @@ public class FileUploadController {
         } catch (IOException e) {
             msgLbl=e.getMessage();
         }
+        treeBean.updateTree();
     }
 
 
