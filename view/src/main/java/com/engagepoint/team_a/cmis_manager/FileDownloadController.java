f1d304786f729eeebf91000f16325ffc694aef42
teamAEngagePoint <kondr_vlad@mail.ru>
2013-12-17 17:03:19 +0200

    Multiple download

--- view/src/main/java/com/engagepoint/team_a/cmis_manager/FileDownloadController.java
+++ view/src/main/java/com/engagepoint/team_a/cmis_manager/FileDownloadController.java
@@ -2,8 +2,11 @@ package com.engagepoint.team_a.cmis_manager;
 
 import com.engagepoint.team_a.cmis_manager.exceptions.BaseException;
 import com.engagepoint.team_a.cmis_manager.model.TypeDTO;
+import com.engagepoint.team_a.cmis_manager.wrappers.TypeDefinitionWrapper;
+import org.apache.chemistry.opencmis.client.util.TypeUtils;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
+import org.primefaces.model.TreeNode;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
@@ -11,8 +14,11 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletContext;
 import javax.xml.stream.XMLStreamException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @ManagedBean
 @SessionScoped
@@ -20,7 +26,7 @@ public class FileDownloadController {
 
     private StreamedContent file;
     private String type;
-    private String[] types = {"xml", "json"};
+    private String[] types = {"xml", "json", "Branch XML", "Branch JSON"};
     @ManagedProperty(value = "#{treeBean}")
     private TreeBean treeBean;
     @ManagedProperty(value = "#{error}")
@@ -59,10 +65,17 @@ public class FileDownloadController {
                 errorBean.setErrorMessage(e.getMessage());
                 errorBean.setErrorVisibility("true");
             }
-        }
+        } else if(type.equals("Branch XML")){
+            getTypeWithAllChildrenXML();
 
-        String saveName = currentType.getId() + "." + type;
-        file = new DefaultStreamedContent(stream, "image/jpg", saveName);
+       } else if (type.equals("Branch JSON")){
+           getTypeWithAllChildrenJSON();
+       }
+
+        if(type.equals("json") || type.equals("xml")){
+            String saveName = currentType.getId() + "." + type;
+            file = new DefaultStreamedContent(stream, "image/jpg", saveName);
+        }
 
         try {
             if(stream != null){
@@ -73,6 +86,136 @@ public class FileDownloadController {
             }
 
     }
+
+
+    private ArrayList<TypeDTO> listOfChildren = null;
+    private void findAllTypeChildren(List<TreeNode> list){
+        for (int i = 0; i < list.size(); i++) {
+            listOfChildren.add((TypeDTO)list.get(i).getData());
+            if(list.get(i).getChildren() != null){
+                findAllTypeChildren(list.get(i).getChildren());
+            }else {
+                return;
+            }
+
+        }
+
+    }
+
+
+    public void getTypeWithAllChildrenXML(){
+        listOfChildren = new ArrayList<TypeDTO>();
+        TreeNode selected = treeBean.getSelected();
+        listOfChildren.add((TypeDTO) selected.getData());
+        if(listOfChildren!=null){
+            findAllTypeChildren(selected.getChildren());
+        }
+        ArrayList<TypeDefinitionWrapper> listOfDefinitions = new ArrayList<TypeDefinitionWrapper>();
+        for (int i = 0; i < listOfChildren.size(); i++) {
+            TypeDefinitionWrapper wrapper = new TypeDefinitionWrapper(listOfChildren.get(i));
+            listOfDefinitions.add(wrapper);
+        }
+
+        ZipOutputStream out = null;
+        String path = FacesContext.getCurrentInstance().getExternalContext().getRealPath("") + "\\types.zip";
+
+        try {
+            out = new ZipOutputStream(new FileOutputStream(new File(path)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        for (int i = 0; i < listOfDefinitions.size(); i++) {
+            OutputStream stream = new ByteArrayOutputStream();
+
+            try {
+                TypeUtils.writeToXML(listOfDefinitions.get(i), stream);
+            } catch (XMLStreamException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+            try {
+                out.putNextEntry(new ZipEntry(listOfDefinitions.get(i).getDisplayName()));
+                out.write(((ByteArrayOutputStream) stream).toByteArray());
+                out.closeEntry();
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        InputStream stream = null;
+        try {
+            stream = new FileInputStream(path);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        file = new DefaultStreamedContent(stream, "image/jpg", "types_in_xml.zip");
+
+
+    }
+
+    public void getTypeWithAllChildrenJSON(){
+        listOfChildren = new ArrayList<TypeDTO>();
+        TreeNode selected = treeBean.getSelected();
+        listOfChildren.add((TypeDTO) selected.getData());
+        if(listOfChildren!=null){
+            findAllTypeChildren(selected.getChildren());
+        }
+        ArrayList<TypeDefinitionWrapper> listOfDefinitions = new ArrayList<TypeDefinitionWrapper>();
+        for (int i = 0; i < listOfChildren.size(); i++) {
+            TypeDefinitionWrapper wrapper = new TypeDefinitionWrapper(listOfChildren.get(i));
+            listOfDefinitions.add(wrapper);
+        }
+
+        ZipOutputStream out = null;
+        String path = FacesContext.getCurrentInstance().getExternalContext().getRealPath("") + "\\types.zip";
+
+        try {
+            out = new ZipOutputStream(new FileOutputStream(new File(path)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        for (int i = 0; i < listOfDefinitions.size(); i++) {
+            OutputStream stream = new ByteArrayOutputStream();
+
+
+            try {
+                TypeUtils.writeToJSON(listOfDefinitions.get(i), stream);
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+
+            try {
+                out.putNextEntry(new ZipEntry(listOfDefinitions.get(i).getDisplayName()));
+                out.write(((ByteArrayOutputStream) stream).toByteArray());
+                out.closeEntry();
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        InputStream stream = null;
+        try {
+            stream = new FileInputStream(path);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        file = new DefaultStreamedContent(stream, "image/jpg", "types_in_json.zip");
+
+
+    }
+
+
 
     public void setTypes(String[] types) {
         this.types = types;
