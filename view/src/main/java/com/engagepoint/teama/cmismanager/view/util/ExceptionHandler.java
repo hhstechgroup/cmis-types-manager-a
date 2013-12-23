1df4dc1742c0c8f07b966e3d53d58ab33d4fb0fa
vishnevskyA <alexander.vishnevsky@engagepoint.com>
2013-12-23 16:03:10 +0200

    fix

--- view/src/main/java/com/engagepoint/teama/cmismanager/view/util/ExceptionHandler.java
+++ view/src/main/java/com/engagepoint/teama/cmismanager/view/util/ExceptionHandler.java
@@ -16,7 +16,6 @@ public class ExceptionHandler extends ExceptionHandlerWrapper {
         this.wrapped = wrapped;
     }
 
-
     @Override
     public javax.faces.context.ExceptionHandler getWrapped() {
         return this.wrapped;
