3e14ea1ef08a09b9ec35fe40628cccd30e5edcd4
ivan.yakubenko <ivan.yakubenko@engagepoint.com>
2014-01-02 13:03:25 +0200

    Fixed SessionEJBTest

--- biz/src/test/java/com/engagepoint/teama/cmismanager/biz/ejb/ObjectTypeReaderTest.java
+++ biz/src/test/java/com/engagepoint/teama/cmismanager/biz/ejb/ObjectTypeReaderTest.java
@@ -1,12 +1,7 @@
 package com.engagepoint.teama.cmismanager.biz.ejb;
 
-/**
- * Created with IntelliJ IDEA.
- * User: aleksandr.davidiuk
- * Date: 12/30/13
- * Time: 5:12 PM
- * To change this template use File | Settings | File Templates.
- */
 public class ObjectTypeReaderTest {
+
+
 
 }
